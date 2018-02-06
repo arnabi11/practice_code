ab494c79a53de5e6df581f2c1ba8a9a42a00059b
arnabi11 <arnabi1112@gmail.com>
2018-02-06 17:08:05 +0530

    dummy push

--- fileWalker.py
+++ fileWalker.py
@@ -3,6 +3,7 @@ import glob
 import sys
 
 def getFileList(path):
+    # return (os.listdir(path))
     pass
 
 def deleteFiles(list):
@@ -12,6 +13,7 @@ def Main():
     fileType = str(raw_input("Enter FileType/FileName: "))
     sourcePath = str(raw_input("Enter Path of the directory: "))
     fileList = getFileList(sourcePath)
+    print fileList
     deleteFiles(fileList)
 
 if __name__=="__main__":
